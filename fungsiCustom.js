e633a12489be1fec9b83193b10caa1f4a57062a5
muthiapry <uciyapriyanti@gmail.com>
2022-06-12 11:06:11 +0700

    Challenge advance callback

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,8 +19,31 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+// let files = [file1, file2, file3];
+const bacaData = (fnCallback) => {
+  const result = [];
 
+  fs.readFile(file1, { encoding: "utf8" }, (err, data) => {
+    const content = JSON.parse(data);
+    message = content.message;
+    result.push(message.split(" ")[1]);
+
+    fs.readFile(file2, { encoding: "utf8" }, (err, data) => {
+      const content = JSON.parse(data);
+      message = content[0].message;
+      result.push(message.split(" ")[1]);
+
+      fs.readFile(file3, { encoding: "utf8" }, (err, data) => {
+        const content = JSON.parse(data);
+        message = content[0].data.message;
+        result.push(message.split(" ")[1]);
+
+        console.log(result);
+        fnCallback(err, result);
+      });
+    });
+  });
+};
 // ! JANGAN DIMODIFIKASI
 module.exports = {
   modifyFile1,
